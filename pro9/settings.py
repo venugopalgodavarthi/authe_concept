9863ae803289aed6dc661e08d2f5ae29f012b3ea
venugopalgodavarthi <venugopalgodavarthi@gmail.com>
2023-11-04 12:59:08 +0000

    secondcommit

--- pro9/settings.py
+++ pro9/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'pro9.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'venugopal$authe',
+        'HOST': 'venugopal.mysql.pythonanywhere-services.com',
+        'USER':'venugopal',
+        'PASSWORD':'Godavarthi@9966'
     }
 }
 
